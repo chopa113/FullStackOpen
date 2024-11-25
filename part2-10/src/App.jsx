1b3260f4c9cc3eca292c859539da552256c480f9
Nikita Balajew <102317425+chopa113@users.noreply.github.com>
2024-11-25 10:06:35 +0100

    added part2-10

--- part2-10/src/App.jsx
+++ part2-10/src/App.jsx
@@ -1,33 +1,96 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
-const Person = ({ person }) => {
-  return <p>{person.name}</p>
-}
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with{' '}
+      <input type="text" value={filter} onChange={handleFilterChange} />
+    </div>
+  );
+};
+
+const PersonForm = ({ handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input type="text" name="name" />
+      </div>
+      <div>
+        number: <input type="text" name="number" />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons, handleClick }) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.id}>
+          {person.name} {person.number}
+          <button name="delete" onClick={handleClick()}>delete</button>
+        </p>
+      ))}
+    </div>
+  );
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+      .then((response) => response.json())
+      .then((data) => setPersons(data));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newName = e.target.elements.name.value;
+    const newNumber = e.target.elements.number.value;
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons([
+        ...persons,
+        { name: newName, number: newNumber, id: persons.length + 1 },
+      ]);
+      e.target.elements.name.value = '';
+      e.target.elements.number.value = '';
+    }
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleDelete = (id) => {
+    setPersons(persons.filter((person) => person.id !== id));
+  }
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      debug: {newName}
-      <h2>Numbers</h2>
-      {persons.map(person => (
-        <Person key={person.name} person={person} />
-      ))}
-    </div>
-  )
-}
 
-export default App
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>add a new</h3>
+
+      <PersonForm handleSubmit={handleSubmit} />
+
+      <Persons persons={filteredPersons} />
+
+    </div>
+  );
+};
+
+export default App;
