911d60e60ced9352be57be1e7f133a78fb4fadc5
Nikita Balajew <102317425+chopa113@users.noreply.github.com>
2025-02-27 22:19:20 +0100

    mongo js

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,13 +1,39 @@
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 
-const url = "mongodb://localhost:3001/";
+if (process.argv.length < 3) {
+    console.log('give password as argument');
+    process.exit(1);
+}
 
-const password = "fnYOxOdrv3Y7Mq3m";
+const url = `mongodb+srv://chopa113:${process.argv[2]}@cluster0.c1kqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.set('strictQuery', false);
 
-const mongodburl = `mongodb+srv://nbalaev231:<db_password>@cluster0.b4ppc.mongodb.net/`;
+mongoose.connect(url);
 
-MongoClient.connect(mongodburl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-    console.log("Database created!");
-    db.close();
-    }
-);
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String  
+})
+
+const Person = new mongoose.model('Person', personSchema)
+
+if(process.argv.length > 3){
+    const NewPerson = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    });
+    
+    NewPerson.save().then(result => {
+        console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`);
+        mongoose.connection.close()
+    });
+}else{
+    Person.find({}).then(result=>{
+        console.log('phonebook:')
+        result.forEach(person =>{
+            console.log(person.name , person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
